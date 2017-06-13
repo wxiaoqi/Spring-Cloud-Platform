5c9af5a70f6692c6b1366fcf0ed951df3b5d83a6
wanghaobin <463540703@qq.com>
2017-06-13 08:57:42 +0800

    [Feature] 完善登出

--- ace-gate/src/main/java/com/github/wxiaoqi/security/gate/config/WebSecurityConfig.java
+++ ace-gate/src/main/java/com/github/wxiaoqi/security/gate/config/WebSecurityConfig.java
@@ -14,22 +14,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//  @Value("${spring.profiles}")
-//  private String env;
+  // @Value("${spring.profiles}")
+  // private String env;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.formLogin().loginPage("/login").defaultSuccessUrl("/admin/index").permitAll().and()
-        .logout().logoutUrl("/logout").and().authorizeRequests()
-        .antMatchers("/**/*.css", "/img/**", "/api/**","/**/*.js") // 放开"/api/**"：为了给被监控端免登录注册
+        .logout().logoutSuccessUrl("/login")
+            .invalidateHttpSession(true) .and().authorizeRequests()
+        .antMatchers("/**/*.css", "/img/**", "/api/**", "/**/*.js") // 放开"/api/**"：为了给被监控端免登录注册
         .permitAll().and().authorizeRequests().antMatchers("/**").authenticated();
     http.csrf().disable();
     http.headers().frameOptions().disable();
     http.httpBasic();
   }
 
- /* @Autowired // 也可以在application.yml文件中配置登录账号密码：security.user.name/password
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("whb").password("").roles("USER");
-  }*/
+  /*
+   * @Autowired // 也可以在application.yml文件中配置登录账号密码：security.user.name/password public void
+   * configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+   * auth.inMemoryAuthentication().withUser("whb").password("").roles("USER"); }
+   */
 }
