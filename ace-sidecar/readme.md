108d615d65a19b1f6147106e28c097cfce6de0bd
superjery <9322512@qq.com>
2017-10-23 08:17:54 +0800

    修正Sidecar名称

--- ace-sidecar/readme.md
+++ ace-sidecar/readme.md
@@ -1,4 +1,4 @@
-使用Sidercar代理远程Python服务，端口为Python的Web端口
+使用Sidecar代理远程Python服务，端口为Python的Web端口
 当然，换成Node的Web端口，即可代理Node的远程服务
 远程服务需要实现一个接口，返回
 
--- ace-sidecar/src/main/java/com/github/wxiaoqi/security/sidecar/SidercarBootstrap.java
+++ ace-sidecar/src/main/java/com/github/wxiaoqi/security/sidecar/SidercarBootstrap.java
@@ -1,4 +1,4 @@
-package com.github.wxiaoqi.security.sidercar;
+package com.github.wxiaoqi.security.sidecar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
