356f1cfd6ea556022a1bd9c8712e45cddde0e407
王浩彬 <mail2ace@163.com>
2017-11-01 21:26:56 +0800

    [Feature] 修改auth数据库名

--- ace-auth/ace-auth-server/src/db/init.sql
+++ ace-auth/ace-auth-server/src/db/init.sql
@@ -1,43 +1,6 @@
 
-CREATE DATABASE ag_gate DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-Use ag_gate;
-
--- ----------------------------
---  Table structure for `base_element`
--- ----------------------------
-DROP TABLE IF EXISTS `base_element`;
-CREATE TABLE `base_element` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` varchar(255) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `uri` varchar(255) DEFAULT NULL,
-  `menu_id` varchar(255) DEFAULT NULL,
-  `parent_id` varchar(255) DEFAULT NULL,
-  `path` varchar(2000) DEFAULT NULL,
-  `method` varchar(10) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `crt_time` datetime DEFAULT NULL,
-  `crt_user` varchar(255) DEFAULT NULL,
-  `crt_name` varchar(255) DEFAULT NULL,
-  `crt_host` varchar(255) DEFAULT NULL,
-  `attr1` varchar(255) DEFAULT NULL,
-  `attr2` varchar(255) DEFAULT NULL,
-  `attr3` varchar(255) DEFAULT NULL,
-  `attr4` varchar(255) DEFAULT NULL,
-  `attr5` varchar(255) DEFAULT NULL,
-  `attr6` varchar(255) DEFAULT NULL,
-  `attr7` varchar(255) DEFAULT NULL,
-  `attr8` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
---  Records of `base_element`
--- ----------------------------
-BEGIN;
-INSERT INTO `base_element` VALUES ('44', 'user:system', 'uri', '获取用户系统', '/api/user/un/{*}/system', '-1', null, null, 'GET', '', null, null, null, null, null, null, null, null, null, null, null, null);
-COMMIT;
+CREATE DATABASE ag_auth DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+Use ag_auth;
 
 -- ----------------------------
 --  Table structure for `gate_client`
--- ace-auth/ace-auth-server/src/main/java/com/github/wxiaoqi/security/auth/feign/IUserService.java
+++ ace-auth/ace-auth-server/src/main/java/com/github/wxiaoqi/security/auth/feign/IUserService.java
@@ -1,14 +1,11 @@
 package com.github.wxiaoqi.security.auth.feign;
 
-import com.github.wxiaoqi.security.api.vo.authority.PermissionInfo;
 import com.github.wxiaoqi.security.api.vo.user.UserInfo;
 import com.github.wxiaoqi.security.auth.configuration.FeignConfiguration;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 
 /**
@@ -19,10 +16,11 @@ import java.util.List;
  */
 @FeignClient(value = "ace-admin",configuration = FeignConfiguration.class)
 public interface IUserService {
+  /**
+   * 根据用户名放回用户信息
+   * @param username
+   * @return
+     */
   @RequestMapping(value = "/api/user/username/{username}", method = RequestMethod.GET)
   public UserInfo getUserByUsername(@PathVariable("username") String username);
-  @RequestMapping(value = "/api/user/un/{username}/permissions", method = RequestMethod.GET)
-  public List<PermissionInfo> getPermissionByUsername(@PathVariable("username") String username);
-  @RequestMapping(value = "/api/permissions", method = RequestMethod.GET)
-  List<PermissionInfo> getAllPermissionInfo();
 }
